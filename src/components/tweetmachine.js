1ae3af9032180e9c9546cc793f91a1cd88c21f7a
Andrew <andrew@digigrow.ie>
2021-11-06 22:24:30 +0000

    Algorithm is now doing a lot more

--- src/components/tweetmachine.js
+++ src/components/tweetmachine.js
@@ -8,18 +8,38 @@ import "../styles/components/tweetMachine/tweetMachine.scss";
 
 export const TweetMachine = () => {
     const [inputTweet, setInputTweet] = useState("");
+    const [running, setRunning] = useState(false);
 
+    const handler = (tweet) => {
+        if (running) return;
 
+        const output = document.getElementById("output");
+
+        setRunning(true);
+        const interval = Math.random() * (2500 - 500) + 500;
+        setTimeout(() => {
+            if (tweet !== inputTweet) {
+                handler(inputTweet);
+            } else {
+                output.value = processTweet(inputTweet);
+                console.log("Delay: " + interval);
+                setRunning(false);
+            }
+        }, interval);
+    };
+
+    //We make sure to add both inputTweet and handler
     useEffect(() => {
         // const input = document.getElementById("input");
         const output = document.getElementById("output");
 
+        //If the input box is populated, add the hashtag
         if (inputTweet) {
-            output.value = processTweet(inputTweet);
+            handler(inputTweet);
         } else {
             output.value = "";
         }
-    }, [inputTweet]);
+    }, [inputTweet, handler]);
 
     return (
         <div className="tweet-machine">
